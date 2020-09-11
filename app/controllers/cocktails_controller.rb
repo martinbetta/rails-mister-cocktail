4da9e7f487233519be18363ff13e7fbe6d4210f7
Martin Bettaglio <martin.bettaglio@gmail.com>
2020-09-11 12:48:02 +0200

    cocktails back-end works, without front-end

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,38 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail.id), notice: 'Cocktail was successfully created.'
+    else
+      render :new
+    end
+  end
+
+
+  private
+#   # Use callbacks to share common setup or constraints between actions.
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
+
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,34 @@
+class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(doses_params)
+
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    dose = Dose.find(params[:id])
+    dose.destroy
+
+    cocktail = dose.cocktail
+    redirect_to cocktail_path(cocktail)
+  end
+
+  private
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+end
